129870e4ff6cdb5b76a54f44e7d16802ee005cf8
Evgeniya Ostapenko <evaostapenko.2707@gmail.com>
2021-06-06 21:24:28 +0300

    new feature

--- Task.java
+++ Task.java
@@ -7,6 +7,34 @@ import java.util.Scanner;
 
 public class Task {
 
+    private static long mergeAndCount(int[] arr, int l,
+                                      int m, int r)
+    {
+
+
+        int[] left = Arrays.copyOfRange(arr, l, m + 1);
+
+
+        int[] right = Arrays.copyOfRange(arr, m + 1, r + 1);
+
+        int i = 0, j = 0, k = l;
+        long swaps = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j])
+                arr[k++] = left[i++];
+            else {
+                arr[k++] = right[j++];
+                swaps += (m + 1) - (l + i);
+            }
+        }
+        while (i < left.length)
+            arr[k++] = left[i++];
+        while (j < right.length)
+            arr[k++] = right[j++];
+        return swaps;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int length = scanner.nextInt();
